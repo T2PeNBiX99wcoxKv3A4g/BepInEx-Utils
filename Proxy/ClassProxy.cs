6b634a8d02b47e7d5b6393323f8cdb61ef944003
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-10 13:32:27 +0800

    refactor(ClassProxy): Replace `Instance` with `_internalInstance` for clarity and encapsulation
    - Mark `MethodAccess` methods as obsolete and recommend `MethodInvoke` as a replacement.
    - Add `[UsedImplicitly]` and `[Obsolete]` attributes where applicable.
    - Improve null checks and exception handling for instance initialization.
    - Remove redundant logic and simplify method invocation infrastructure.

--- Proxy/ClassProxy.cs
+++ Proxy/ClassProxy.cs
@@ -1,24 +1,22 @@
+using BepInExUtils.Extensions;
 using HarmonyLib;
-
-// ReSharper disable UnusedType.Global
-// ReSharper disable UnusedMember.Global
+using JetBrains.Annotations;
 
 namespace BepInExUtils.Proxy;
 
+[UsedImplicitly]
 public abstract class ClassProxy
 {
-    // ReSharper disable once MemberCanBePrivate.Global
-    protected readonly object? Instance;
+    private readonly object? _internalInstance;
 
-    // ReSharper disable once MemberCanBePrivate.Global
-    protected readonly Type Type;
+    [UsedImplicitly] protected readonly Type Type;
 
     protected ClassProxy(object? instance, string className)
     {
         if (string.IsNullOrEmpty(className))
             throw new ArgumentException("Class name cannot be null or empty", className);
         Type = AccessTools.TypeByName(className);
-        Instance = instance;
+        _internalInstance = instance ?? throw new NullReferenceException("instance is null");
     }
 
     protected ClassProxy(string className)
@@ -27,7 +25,8 @@ public abstract class ClassProxy
             throw new ArgumentException("Class name cannot be null or empty", className);
         Type = AccessTools.TypeByName(className);
         if (Type.IsAbstract) return;
-        Instance = Activator.CreateInstance(Type) ?? throw new NullReferenceException("Could not create instance");
+        _internalInstance = Activator.CreateInstance(Type) ??
+                            throw new NullReferenceException("Could not create instance");
     }
 
     protected ClassProxy(string className, params object[] args)
@@ -35,8 +34,9 @@ public abstract class ClassProxy
         if (string.IsNullOrEmpty(className))
             throw new ArgumentException("Class name cannot be null or empty", className);
         Type = AccessTools.TypeByName(className);
-        Instance = typeof(Activator).GetMethod(nameof(Activator.CreateInstance))?.Invoke(Instance, args) ??
-                   throw new NullReferenceException("Could not create instance");
+        _internalInstance =
+            typeof(Activator).GetMethod(nameof(Activator.CreateInstance))?.Invoke(_internalInstance, args) ??
+            throw new NullReferenceException("Could not create instance");
     }
 
     protected ClassProxy(string className, object[] args, object[] activationAttributes)
@@ -44,25 +44,22 @@ public abstract class ClassProxy
         if (string.IsNullOrEmpty(className))
             throw new ArgumentException("Class name cannot be null or empty", className);
         Type = AccessTools.TypeByName(className);
-        Instance = Activator.CreateInstance(Type, args, activationAttributes) ??
-                   throw new NullReferenceException("Could not create instance");
+        _internalInstance = Activator.CreateInstance(Type, args, activationAttributes) ??
+                            throw new NullReferenceException("Could not create instance");
     }
 
-    // ReSharper disable once UnusedMember.Global
-    protected T? MethodAccess<T>(string methodName, params object?[] parameters)
-    {
-        var method = AccessTools.Method(Type, methodName);
-        if (method == null)
-            throw new MethodAccessException($"Method {methodName} not found.");
-        var result = method.Invoke(Instance, parameters);
-        return result is T a ? a : default;
-    }
+    [UsedImplicitly]
+    protected object Instance => _internalInstance ?? throw new NullReferenceException("instance is null");
 
+    [Obsolete("Use object.MethodInvoke instead.")]
+    [UsedImplicitly]
+    protected T? MethodAccess<T>(string methodName, params object?[] parameters) =>
+        Instance.MethodInvoke<T>(methodName, parameters);
+
+    [Obsolete("Use object.MethodInvoke instead.")]
+    [UsedImplicitly]
     protected void MethodAccess(string methodName, params object?[] parameters)
     {
-        var method = AccessTools.Method(Type, methodName);
-        if (method == null)
-            throw new MethodAccessException($"Method {methodName} not found.");
-        method.Invoke(Instance, parameters);
+        Instance.MethodInvoke(methodName, parameters);
     }
 }
