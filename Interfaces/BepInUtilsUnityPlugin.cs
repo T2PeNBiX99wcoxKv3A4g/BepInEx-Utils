69f387cd2936489a1bf35492e5000f9f7951f6f9
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-16 05:46:05 +0800

    fix(Interfaces): mark `BepInUtilsUnityPlugin` as obsolete with guidance to use `IBepInUtils` instead

--- Interfaces/BepInUtilsUnityPlugin.cs
+++ Interfaces/BepInUtilsUnityPlugin.cs
@@ -4,6 +4,7 @@ using JetBrains.Annotations;
 namespace BepInExUtils.Interfaces;
 
 [PublicAPI]
+[Obsolete("Use IBepInUtils instead")]
 public abstract class BepInUtilsUnityPlugin : BaseUnityPlugin
 {
     protected abstract void PostAwake();
