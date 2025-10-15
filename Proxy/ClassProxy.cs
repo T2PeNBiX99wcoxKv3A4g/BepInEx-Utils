e8bc789ecd0f599a0b781efe5208a44be9e297f3
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-15 09:13:54 +0800

    refactor(ClassProxy): standardize exception argument naming and enhance class consistency
    - Replaced incorrect argument name references with `nameof` for better clarity and maintainability.
    - Added `ClassName` field to constructors for improved consistency.
    - Replaced `[UsedImplicitly]` with `[PublicAPI]` for public-facing APIs.
    - Made minor cleanups for property and attribute formatting.

--- Proxy/ClassProxy.cs
+++ Proxy/ClassProxy.cs
@@ -3,27 +3,32 @@ using JetBrains.Annotations;
 
 namespace BepInExUtils.Proxy;
 
-[UsedImplicitly]
+[PublicAPI]
 public abstract class ClassProxy
 {
-    [UsedImplicitly] protected readonly Type Type;
+    protected readonly string ClassName;
+    protected readonly Type Type;
 
     protected ClassProxy(object? instance, string className)
     {
         if (string.IsNullOrEmpty(className))
-            throw new ArgumentException("Class name cannot be null or empty", className);
+            throw new ArgumentException("Class name cannot be null or empty", nameof(className));
         Type = AccessTools.TypeByName(className);
         if (Type == null)
             throw new TypeAccessException($"Type {className} not found.");
         if (Type.BaseType == GetType())
             throw new TypeAccessException($"Type {className} is subclass of {GetType().Name}.");
+        var instanceType = instance?.GetType();
+        if (instanceType != Type)
+            throw new TypeAccessException($"instance {instanceType?.FullName} is not match {Type.FullName}.");
+        ClassName = className;
         Native = instance ?? throw new NullReferenceException("instance is null");
     }
 
     protected ClassProxy(string className)
     {
         if (string.IsNullOrEmpty(className))
-            throw new ArgumentException("Class name cannot be null or empty", className);
+            throw new ArgumentException("Class name cannot be null or empty", nameof(className));
         Type = AccessTools.TypeByName(className);
         if (Type == null)
             throw new TypeAccessException($"Type {className} not found.");
@@ -31,6 +36,7 @@ public abstract class ClassProxy
             throw new TypeAccessException($"Type {className} is subclass of {GetType().Name}.");
         if (Type.IsAbstract)
             throw new TypeAccessException($"Type {className} is abstract class.");
+        ClassName = className;
         Native = Activator.CreateInstance(Type) ??
                  throw new NullReferenceException("Could not create instance");
     }
@@ -38,7 +44,7 @@ public abstract class ClassProxy
     protected ClassProxy(string className, params object[] args)
     {
         if (string.IsNullOrEmpty(className))
-            throw new ArgumentException("Class name cannot be null or empty", className);
+            throw new ArgumentException("Class name cannot be null or empty", nameof(className));
         Type = AccessTools.TypeByName(className);
         if (Type == null)
             throw new TypeAccessException($"Type {className} not found.");
@@ -46,6 +52,7 @@ public abstract class ClassProxy
             throw new TypeAccessException($"Type {className} is subclass of {GetType().Name}.");
         if (Type.IsAbstract)
             throw new TypeAccessException($"Type {className} is abstract class.");
+        ClassName = className;
         Native = Activator.CreateInstance(Type, args) ??
                  throw new NullReferenceException("Could not create instance");
     }
@@ -53,7 +60,7 @@ public abstract class ClassProxy
     protected ClassProxy(string className, object[] args, object[] activationAttributes)
     {
         if (string.IsNullOrEmpty(className))
-            throw new ArgumentException("Class name cannot be null or empty", className);
+            throw new ArgumentException("Class name cannot be null or empty", nameof(className));
         Type = AccessTools.TypeByName(className);
         if (Type == null)
             throw new TypeAccessException($"Type {className} not found.");
@@ -61,13 +68,12 @@ public abstract class ClassProxy
             throw new TypeAccessException($"Type {className} is subclass of {GetType().Name}.");
         if (Type.IsAbstract)
             throw new TypeAccessException($"Type {className} is abstract class.");
+        ClassName = className;
         Native = Activator.CreateInstance(Type, args, activationAttributes) ??
                  throw new NullReferenceException("Could not create instance");
     }
 
-    [Obsolete("Use Native instead.")]
-    [UsedImplicitly]
-    protected object Instance => Native;
+    [Obsolete("Use Native instead.")] protected object Instance => Native;
 
-    [UsedImplicitly] public object Native { get; }
+    public object Native { get; }
 }
