61e809fb41016421f3a12fe28f2f15604c633006
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-15 13:41:15 +0800

    chore(Main): fix version

--- Main.cs
+++ Main.cs
@@ -6,7 +6,7 @@ namespace BepInExUtils;
 [BepInUtils("io.github.ykysnk.BepinExUtils", "BepInEx Utils", Version)]
 public partial class Main
 {
-    private const string Version = "0.0.1";
+    private const string Version = "0.8.0";
 
     protected override void PostAwake()
     {
