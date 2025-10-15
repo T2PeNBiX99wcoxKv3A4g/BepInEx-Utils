67568e0ecd7371d3207187f137669d4c1ce0f359
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-15 13:21:51 +0800

    fix(Main): change name, load `CommandManager` when awake

--- Main.cs
+++ Main.cs
@@ -1,13 +1,15 @@
 using BepInExUtils.Attributes;
+using BepInExUtils.Commands;
 
 namespace BepInExUtils;
 
-[BepInUtils("io.github.ykysnk.BepinExUtils", "BepInExUtils", Version)]
+[BepInUtils("io.github.ykysnk.BepinExUtils", "BepInEx Utils", Version)]
 public partial class Main
 {
     private const string Version = "0.0.1";
 
     protected override void PostAwake()
     {
+        CommandManager.Init();
     }
 }
