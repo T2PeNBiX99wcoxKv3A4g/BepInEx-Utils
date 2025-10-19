e6393dd6bc806859a8f3f84642c81f641522921c
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-19 20:57:01 +0800

    refactor(MethodUtils): extract `GetCallerName` and `GetMethodName` methods for improved reusability and clarity

--- MethodUtils.cs
+++ MethodUtils.cs
@@ -8,28 +8,28 @@ public static class MethodUtils
 {
     private const string UnknownName = "Unknown";
 
-    public static string CallerName
+    public static string CallerName => GetCallerName(2);
+
+    public static string MethodName => GetMethodName(1);
+
+    public static string GetCallerName(int index = 1)
     {
-        get
-        {
-            var stackTrace = new StackTrace();
-            var frame = stackTrace.GetFrame(2);
-            if (frame == null) return UnknownName;
-            var caller = frame.GetMethod();
-            if (caller == null) return UnknownName;
-            return caller.DeclaringType?.Name ?? caller.Name;
-        }
+        var newIndex = index + 1;
+        var stackTrace = new StackTrace();
+        var frame = stackTrace.GetFrame(newIndex);
+        if (frame == null) return UnknownName;
+        var caller = frame.GetMethod();
+        if (caller == null) return UnknownName;
+        return caller.DeclaringType?.Name ?? caller.Name;
     }
 
-    public static string MethodName
+    public static string GetMethodName(int index = 0)
     {
-        get
-        {
-            var stackTrace = new StackTrace();
-            var frame = stackTrace.GetFrame(1);
-            if (frame == null) return UnknownName;
-            var caller = frame.GetMethod();
-            return caller == null ? UnknownName : caller.Name;
-        }
+        var newIndex = index + 1;
+        var stackTrace = new StackTrace();
+        var frame = stackTrace.GetFrame(newIndex);
+        if (frame == null) return UnknownName;
+        var caller = frame.GetMethod();
+        return caller == null ? UnknownName : caller.Name;
     }
 }
