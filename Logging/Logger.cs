f4dff2a9169dd7063aa65bbdc6843459e197c96f
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-19 22:28:08 +0800

    refactor(Logger): centralize log message formatting with reusable constant

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -5,6 +5,7 @@ namespace BepInExUtils.Logging;
 [PublicAPI]
 public class Logger(string sourceName)
 {
+    private const string LogFormat = "[{0}] {1}";
     private AsyncLogSource? _logger;
 
     private AsyncLogSource LogSource => _logger ??= CreateAsyncLogSource(sourceName);
@@ -16,28 +17,65 @@ public class Logger(string sourceName)
         return logSource;
     }
 
-    public void LogError(object data) => _ = LogSource.LogError($"[{MethodUtils.CallerName}] {data}");
-    public async Task LogErrorAsync(object data) => await LogSource.LogError($"[{MethodUtils.CallerName}] {data}");
-    public void LogDebug(object data) => _ = LogSource.LogDebug($"[{MethodUtils.CallerName}] {data}");
-    public async Task LogDebugAsync(object data) => await LogSource.LogDebug($"[{MethodUtils.CallerName}] {data}");
-    public void LogWarning(object data) => _ = LogSource.LogWarning($"[{MethodUtils.CallerName}] {data}");
-    public async Task LogWarningAsync(object data) => await LogSource.LogWarning($"[{MethodUtils.CallerName}] {data}");
-    public void LogFatal(object data) => _ = LogSource.LogFatal($"[{MethodUtils.CallerName}] {data}");
-    public async Task LogFatalAsync(object data) => await LogSource.LogFatal($"[{MethodUtils.CallerName}] {data}");
-    public void LogInfo(object data) => _ = LogSource.LogInfo($"[{MethodUtils.CallerName}] {data}");
-    public async Task LogInfoAsync(object data) => await LogSource.LogInfo($"[{MethodUtils.CallerName}] {data}");
-    public void LogMessage(object data) => _ = LogSource.LogMessage($"[{MethodUtils.CallerName}] {data}");
-    public async Task LogMessageAsync(object data) => await LogSource.LogMessage($"[{MethodUtils.CallerName}] {data}");
-    public void Error(object data) => _ = LogSource.LogError($"[{MethodUtils.CallerName}] {data}");
-    public async Task ErrorAsync(object data) => await LogSource.LogError($"[{MethodUtils.CallerName}] {data}");
-    public void Debug(object data) => _ = LogSource.LogDebug($"[{MethodUtils.CallerName}] {data}");
-    public async Task DebugAsync(object data) => await LogSource.LogDebug($"[{MethodUtils.CallerName}] {data}");
-    public void Warning(object data) => _ = LogSource.LogWarning($"[{MethodUtils.CallerName}] {data}");
-    public async Task WarningAsync(object data) => await LogSource.LogWarning($"[{MethodUtils.CallerName}] {data}");
-    public void Fatal(object data) => _ = LogSource.LogFatal($"[{MethodUtils.CallerName}] {data}");
-    public async Task FatalAsync(object data) => await LogSource.LogFatal($"[{MethodUtils.CallerName}] {data}");
-    public void Info(object data) => _ = LogSource.LogInfo($"[{MethodUtils.CallerName}] {data}");
-    public async Task InfoAsync(object data) => await LogSource.LogInfo($"[{MethodUtils.CallerName}] {data}");
-    public void Message(object data) => _ = LogSource.LogMessage($"[{MethodUtils.CallerName}] {data}");
-    public async Task MessageAsync(object data) => await LogSource.LogMessage($"[{MethodUtils.CallerName}] {data}");
+    public void LogError(object data) => _ = LogSource.LogError(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task LogErrorAsync(object data) =>
+        await LogSource.LogError(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void LogDebug(object data) => _ = LogSource.LogDebug(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task LogDebugAsync(object data) =>
+        await LogSource.LogDebug(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void LogWarning(object data) =>
+        _ = LogSource.LogWarning(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task LogWarningAsync(object data) =>
+        await LogSource.LogWarning(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void LogFatal(object data) => _ = LogSource.LogFatal(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task LogFatalAsync(object data) =>
+        await LogSource.LogFatal(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void LogInfo(object data) => _ = LogSource.LogInfo(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task LogInfoAsync(object data) =>
+        await LogSource.LogInfo(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void LogMessage(object data) =>
+        _ = LogSource.LogMessage(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task LogMessageAsync(object data) =>
+        await LogSource.LogMessage(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void Error(object data) => _ = LogSource.LogError(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task ErrorAsync(object data) =>
+        await LogSource.LogError(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void Debug(object data) => _ = LogSource.LogDebug(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task DebugAsync(object data) =>
+        await LogSource.LogDebug(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void Warning(object data) => _ = LogSource.LogWarning(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task WarningAsync(object data) =>
+        await LogSource.LogWarning(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void Fatal(object data) => _ = LogSource.LogFatal(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task FatalAsync(object data) =>
+        await LogSource.LogFatal(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void Info(object data) => _ = LogSource.LogInfo(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task InfoAsync(object data) =>
+        await LogSource.LogInfo(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public void Message(object data) => _ = LogSource.LogMessage(string.Format(LogFormat, MethodUtils.CallerName, data));
+
+    public async Task MessageAsync(object data) =>
+        await LogSource.LogMessage(string.Format(LogFormat, MethodUtils.CallerName, data));
 }
