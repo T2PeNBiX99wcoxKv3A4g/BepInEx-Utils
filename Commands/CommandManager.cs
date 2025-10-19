a90bc4869fdfb74f5e2e4e76d0891378e1dd2ac1
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-19 20:57:29 +0800

    feat(CommandManager): make commands asynchronous by updating delegate and execution logic

--- Commands/CommandManager.cs
+++ Commands/CommandManager.cs
@@ -5,18 +5,18 @@ namespace BepInExUtils.Commands;
 [PublicAPI]
 public static class CommandManager
 {
-    public delegate void Command(string[] args);
+    public delegate Task Command(string[] args);
 
     private static readonly Dictionary<string, CommandInfo> Infos = [];
 
     private static readonly List<CommandInfo> DefaultCommands =
     [
-        new("help", "Show command infos", _ =>
+        new("help", "Show command infos", async _ =>
         {
             var cmdInfos = Infos.Values.Select(cmd => $"{cmd.Name} - {cmd.Description}");
-            Utils.Logger.Info($"Available commands:\n{string.Join("\n", cmdInfos)}");
+            await Utils.Logger.InfoAsync($"Available commands:\n{string.Join("\n", cmdInfos)}");
         }),
-        new("echo", "Echo args", args => Utils.Logger.Info(string.Join("\n", args)))
+        new("echo", "Echo args", async args => { await Utils.Logger.InfoAsync(string.Join("\n", args)); })
     ];
 
     internal static void Init()
@@ -30,10 +30,10 @@ public static class CommandManager
     public static void AddCommand(string commandName, string description, Command command) =>
         AddCommand(new(commandName, description, command));
 
-    public static bool TryExecuteCommand(string command, params string[] args)
+    public static async Task<bool> TryExecuteCommand(string command, params string[] args)
     {
         if (!Infos.TryGetValue(command, out var info)) return false;
-        info.Command(args);
+        await info.Command(args);
         return true;
     }
 }
